dd2f9d4eb49ea43ff75c28e4d265d5b11f99d968
jjl12138 <2516638426@qq.com>
2020-04-20 10:47:30 +0800

    博客园

--- cnblog/pipelines.py
+++ cnblog/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+# file
 class FilePipeline(object):
     '''
     实现保存到txt文件的类，类名这个地方为了区分，做了修改，
